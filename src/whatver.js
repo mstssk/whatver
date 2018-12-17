7604651c63dde43dfd62b242ecfa17d1fe2c5c5c
mstssk <mstssk+github@gmail.com>
2018-12-18 02:14:13 +0900

    prototype

---
+++ src/whatver.js
@@ -0,0 +1,73 @@
+const https = require('https');
+const { spawn } = require('child_process');
+
+const DEFAULT_REPO = "https://raw.githubusercontent.com/mstssk/whatver-repo/master";
+
+if (process.argv.length < 3) {
+    console.error('missing <command>');
+    process.exit(2);
+} else {
+    main(process.argv[2]);
+}
+
+
+/**
+ * @param {string} command
+ */
+async function main(command) {
+    command = sanitizeCommand(command);
+    const verArg = await resolveVerArg(command);
+    const cmd = spawn(command, [verArg]);
+    cmd.stdout.on('data', data => console.log(data.toString('utf8').trim()));
+    cmd.stderr.on('data', data => console.error(data.toString('utf8').trim()));
+    cmd.on('close', code => process.exit(code));
+}
+
+function sanitizeCommand(command) {
+    return command; // FIXME
+}
+
+/**
+ * @param {string} command
+ * @returns {string} verArg
+ */
+async function resolveVerArg(command) {
+    // TODO result cache
+    const url = resolveUrl(command);
+    const responseBody = await httpsGet(url);
+    const result = JSON.parse(responseBody);
+    if (!result['verarg']) {
+        throw new Error('missing verarg');
+    }
+    return result['verarg'];
+}
+
+/**
+ * @param {string} command
+ * @returns {string} url
+ */
+function resolveUrl(command) {
+    const repo = DEFAULT_REPO;
+    const initial = command.charAt(0).toLowerCase();
+    return `${repo}/data/${initial}/${command}.json`;
+}
+
+/**
+ * @param {string} url
+ * @returns {Promise<string>} result
+ */
+function httpsGet(url) {
+    return new Promise((resolve, rejetct) => {
+        https.get(url, res => {
+            if (res.statusCode != 200) {
+                throw new Error(`${res.statusCode} : ${url}`);
+            }
+            let result = "";
+            res.setEncoding("utf8");
+            res.on('data', chunk => result += chunk);
+            res.on("end", () => resolve(result));
+        }).on("error", e => {
+            rejetct(e);
+        });
+    })
+}
